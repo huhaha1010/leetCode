b4dcf4e4872854fdf0961ed6c4f8f13049936707
hhl1010 <hhl02281101@163.com>
2019-06-25 10:47:40 +0800

    冒泡排序

---
+++ src/code190613/BubbleSort.java
@@ -0,0 +1,27 @@
+package code190613;
+
+import java.util.Scanner;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int p = 0; p < n; p++) {
+            int num = sc.nextInt();
+            arr[p] = num;
+        }
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] < arr[j + 1]) {
+                    int t = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = t;
+                }
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
